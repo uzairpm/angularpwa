798415f9194b712e515481aef24f22fda894b431
Uzair Ahmed <uzair_ahmed_peer_mohammed@epam.com>
2018-09-25 17:08:42 +0200

    Added support for angular material and PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from  './api.service';
+import { Item } from  './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularpwa';
+  items:  Array<Item>;
+  constructor(private  apiService:  ApiService){
+  }
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+  fetchData(): any {
+    this.apiService.fetch().subscribe((data: Array<Item>) =>{
+      console.log(data);
+      this.items  =  data;
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from  '@angular/common/http';
+
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatCardModule } from  '@angular/material/card';
+import { MatButtonModule } from  '@angular/material/button';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
